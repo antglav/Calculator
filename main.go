92df3da30a97db94f0b83840f7314cb6dd4d8990
antglav <154250540+antglav@users.noreply.github.com>
2026-09-18 20:24:29 +0300

    feat: add PATCH and DELETE endpoint - add PATCH \calculations endpoint to fetch history - add DELETE \calculations endpoint to fetch history

--- main.go
+++ main.go
@@ -56,6 +56,38 @@ func postCalculations(c echo.Context) error {
 	return c.JSON(http.StatusCreated, calc)
 }
 
+func patchCalculations(c echo.Context) error {
+	id := c.Param("id")
+	var req CalculationRequest
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request"})
+	}
+	result, err := calculateExpression(req.Expression)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid expression"})
+	}
+	for i, calculation := range calculations {
+		if calculation.ID == id {
+			calculations[i].Expression = req.Expression
+			calculations[i].Result = result
+			return c.JSON(http.StatusOK, calculations[i])
+		}
+	}
+	return c.JSON(http.StatusBadRequest, map[string]string{"error": "Calculation not found"})
+}
+
+func deleteCalculations(c echo.Context) error {
+	id := c.Param("id")
+
+	for i, calculation := range calculations {
+		if calculation.ID == id {
+			calculations = append(calculations[:i], calculations[i+1:]...)
+			return c.NoContent(http.StatusNoContent)
+		}
+	}
+	return c.JSON(http.StatusBadRequest, map[string]string{"error": "Calculation not found"})
+}
+
 func main() {
 	e := echo.New()
 	e.Use(middleware.CORS())
@@ -63,5 +95,7 @@ func main() {
 
 	e.GET("/calculations", getCalculations)
 	e.POST("/calculations", postCalculations)
+	e.PATCH("/calculations/:id", patchCalculations)
+	e.DELETE("calculations/:id", deleteCalculations)
 	e.Logger.Fatal(e.Start(":8080"))
 }
